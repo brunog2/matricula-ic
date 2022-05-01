74a4c972c25a0ef5c07fc8c3a3d36c112e394f3a
Bruno Gomes <bgbf@ic.ufal.br>
2022-05-01 16:46:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# 2 = mínimo 5 e máximo 6
+# 0 = calouro, formando, fluxo contínuo e fluxo individual
+# 1 = a remoção que atende uma troca; a remoção que atende uma inserção, a remoção, a troca e a inserção
+# 2 = atender os alunos que realizou a menor quantidade de disciplinas
