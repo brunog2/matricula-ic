239be5b5429a13f34f9d53ff5983f5054f506d63
Th0nys <86919567+Th0nys@users.noreply.github.com>
2022-05-08 03:23:00 -0300

    Update student.py

--- modules/student.py
+++ modules/student.py
@@ -1,11 +1,20 @@
 class Student:
-    def __init__(self, name, registration, period, enrolledSubjects):
+    def init(self, name, registration, period, enrolledSubjects, status):
         self.name = name
         self.registration = registration
         self.period = period
         self.enrolledSubjects = enrolledSubjects
+        self.status = status
+
 
     def info(self):
+        if self.period == 1:
+            self.status = 'calouro'
+        elif self.period >= 6:
+            self.status = 'formando'
+        elif self.period:
+            pass
+
         subjects = [subject.info() for subject in self.enrolledSubjects]
-        output = {'Nome': self.name, 'Matrícula': self.registration, 'Período': self.period, 'Disciplinas matriculadas': subjects}
+        output = {'Nome': self.name, 'Matrícula': self.registration, 'Período': self.period, 'Disciplinas matriculadas': subjects, 'Status': self.status}
         return output
